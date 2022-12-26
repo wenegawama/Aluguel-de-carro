acce089a8750c83623dcc6d063d0936f437a6420
Wenega Wama <narcissewatherloy@gmail.com>
2022-12-26 09:15:41 -0300

    Added inversion dependency principe of solid

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express"
 import { CategoriesRepository } from "../repositories/CategoriesRepository"
+import { CreateCategoryService } from "../services/CreateCategoryService"
 
 
 const categoriesRoutes = Router()
@@ -8,7 +9,8 @@ const categoriesRepository = new CategoriesRepository()
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body
 
-    
+    const createCategoryService = new CreateCategoryService(categoriesRepository)  
+    createCategoryService.execute({ name,description})  //Separei a responsabilidade e deixei para o servico
     return response.status(201).send()
 })
 
